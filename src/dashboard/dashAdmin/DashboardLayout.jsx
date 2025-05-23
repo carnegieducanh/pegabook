19b6994719f70f41f51bdc0e4fc15b3468a255ca
carnegie duc anh <huyducanh@gmail.com>
2025-05-23 12:09:36 +0900

    Edit 05/23

--- src/dashboard/dashAdmin/DashboardLayout.jsx
+++ src/dashboard/dashAdmin/DashboardLayout.jsx
@@ -1,13 +1,12 @@
 import { Outlet } from "react-router-dom";
 import DashSidebar from "./DashSidebar";
 import { Suspense } from "react";
-import SpinnerLoading from "../../components/SpinnerLoading";
 
 const DashboardLayout = () => {
   return (
     <div className="flex flex-col gap-4 lg:flex-row">
       <DashSidebar />
-      <Suspense fallback={<SpinnerLoading />}>
+      <Suspense>
         <Outlet />
       </Suspense>
     </div>
--- src/dashboard/dashSharer/DashSharerLayout.jsx
+++ src/dashboard/dashSharer/DashSharerLayout.jsx
@@ -1,13 +1,12 @@
 import { Outlet } from "react-router-dom";
 import DashSharerSidebar from "./DashSharerSidebar";
 import { Suspense } from "react";
-import SpinnerLoading from "../../components/SpinnerLoading";
 
 const DashSharerLayout = () => {
   return (
     <div className="flex flex-col lg:flex-row lg:gap-4">
       <DashSharerSidebar />
-      <Suspense fallback={<SpinnerLoading />}>
+      <Suspense>
         <Outlet />
       </Suspense>
     </div>
