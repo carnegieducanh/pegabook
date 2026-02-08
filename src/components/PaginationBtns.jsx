9602a0d6bfa8dba92841b4ee41306ae74e569201
Huy Duc Anh <huyducanh@gmail.com>
2026-02-08 11:21:12 +0900

    Sửa lại kích thước nút chuyển trang trên mobile

--- src/components/PaginationBtns.jsx
+++ src/components/PaginationBtns.jsx
@@ -1,45 +1,45 @@
-import React from "react";
 import ReactPaginate from "react-paginate";
 import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
 
+// eslint-disable-next-line react/prop-types
 const PaginationButtons = ({ booksPerPage, totalBooks, paginate }) => {
-    const handlePageClick = ({ selected }) => {
-        const pageNumber = selected + 1;
-        paginate(pageNumber);
-    };
+  const handlePageClick = ({ selected }) => {
+    const pageNumber = selected + 1;
+    paginate(pageNumber);
+  };
 
-    return (
-        <div className="my-10">
-            <ReactPaginate
-                breakLabel={<span className="mr-4">...</span>}
-                nextLabel={
-                    <span className="w-24 h-10 text-base flex items-center justify-center text-white bg-[#a69060] hover:bg-gray-500 rounded-md ">
-                        Next
-                        <BsChevronRight />
-                    </span>
-                }
-                onPageChange={handlePageClick}
-                pageRangeDisplayed={3}
-                marginPagesDisplayed={2}
-                pageCount={Math.ceil(totalBooks / booksPerPage)}
-                previousLabel={
-                    <span className="w-24 h-10 text-base flex items-center justify-center text-white bg-[#a69060] hover:bg-gray-500 rounded-md mr-4 ">
-                        Previous <BsChevronLeft />
-                    </span>
-                }
-                pageClassName="block bg-solid cursor-pointer text-gray-500 hover:bg-gray-500 hover:text-white w-10 h-10 text-base flex items-center justify-center font-medium rounded-md mr-4"
-                pageLinkClassName="p-4"
-                previousClassName="page-item"
-                previousLinkClassName="page-link"
-                nextClassName="page-item"
-                nextLinkClassName="page-link"
-                breakClassName="page-item"
-                breakLinkClassName="page-link"
-                containerClassName="flex items-center justify-center mt-8"
-                activeClassName=" text-white bg-[#a69060]"
-            />
-        </div>
-    );
+  return (
+    <div className="my-10 px-2">
+      <ReactPaginate
+        breakLabel={<span className="mr-2 sm:mr-4">...</span>}
+        nextLabel={
+          <span className="flex h-8 w-16 items-center justify-center gap-1 rounded-md bg-[#a69060] text-xs text-white hover:bg-gray-500 sm:h-10 sm:w-24 sm:text-base">
+            Next
+            <BsChevronRight />
+          </span>
+        }
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={3}
+        marginPagesDisplayed={2}
+        pageCount={Math.ceil(totalBooks / booksPerPage)}
+        previousLabel={
+          <span className="mr-2 flex h-8 w-16 items-center justify-center gap-1 rounded-md bg-[#a69060] text-xs text-white hover:bg-gray-500 sm:mr-4 sm:h-10 sm:w-24 sm:text-base">
+            Prev <BsChevronLeft />
+          </span>
+        }
+        pageClassName="block bg-solid cursor-pointer text-gray-500 hover:bg-gray-500 hover:text-white w-8 sm:w-10 h-8 sm:h-10 text-xs sm:text-base flex items-center justify-center font-medium rounded-md mr-2 sm:mr-4"
+        pageLinkClassName="p-2 sm:p-4"
+        previousClassName="page-item"
+        previousLinkClassName="page-link"
+        nextClassName="page-item"
+        nextLinkClassName="page-link"
+        breakClassName="page-item"
+        breakLinkClassName="page-link"
+        containerClassName="flex flex-wrap items-center justify-center mt-8 gap-2"
+        activeClassName=" text-white bg-[#a69060]"
+      />
+    </div>
+  );
 };
 
 export default PaginationButtons;
